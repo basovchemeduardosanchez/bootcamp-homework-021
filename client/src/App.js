b73b175e38652ebc5d18c38c81dc521e55af23d1
Eduardo Sánchez Villafán <basov_chem_eduardo_sanchez@hotmail.com>
2020-09-12 12:35:38 -0500

    Frontend updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Search from './pages/Search';
+import Saved from './pages/Saved';
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <BrowserRouter>
+      <Header />
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h1 className="display-4">Google Books Search</h1>
+          <p className="lead">Search Books and Save Books of Interest</p>
+        </div>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+      <Switch>
+        <Route exact path={[`/search`, '/']} >
+          <Search />
+        </Route>
+        <Route exact path={[`/saved`, '/']} >
+          <Saved />
+        </Route>
+      </Switch>
+      <Footer />
+    </BrowserRouter>
   );
 }
-
 
 export default App;
